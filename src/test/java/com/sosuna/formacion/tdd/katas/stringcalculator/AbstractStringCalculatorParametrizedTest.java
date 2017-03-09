345bac7b4c13f0cb0c1df4c34211049f0de31d9a
XYZ <idm.caz@mailinator.com>
2017-03-09 23:41:34 +0100

    Test refactoring.
    Created base class AbstractStringCalculatorParametrizedTest to remove boilerplate from all other test classes.

---
+++ src/test/java/com/sosuna/formacion/tdd/katas/stringcalculator/AbstractStringCalculatorParametrizedTest.java
@@ -0,0 +1,62 @@
+package com.sosuna.formacion.tdd.katas.stringcalculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+
+/**
+ * Abstract parametrized test class.
+ * For implementation of tests, extend it and declare a @Parameters method to
+ * provide test data with the following signature:
+ *
+ * Example:
+ *
+        \@Parameters(name = "{0}")
+        public static Object[][] data() {
+            return new Object[][] {
+                {
+                    "Empty string should return 0",     // This is the test name
+                    "",                                 // This is the test input
+                    0                                   // This test should return normally with this result
+                },
+                {
+                    "Negative numbers should raise an exception",       // This is the test name
+                    "1,2,3",                                            // This is the test input
+                    NegativeNumbersException.class                      // This test should raise an exception of type NegativeNumbersException
+                }
+            };
+        }
+
+ */
+@RunWith(Parameterized.class)
+public abstract class AbstractStringCalculatorParametrizedTest {
+
+    @Parameterized.Parameter(0)
+    public String message;
+    @Parameterized.Parameter(1)
+    public String input;
+    @Parameterized.Parameter(2)
+    public Object expectedResult;
+
+    public AbstractStringCalculatorParametrizedTest() {
+    }
+
+    @Test
+    public void test() {
+        if (expectedResult==null) {
+            throw new IllegalArgumentException("Test must specify an expected result, either an integer or an exception class");
+        }
+        else if (expectedResult instanceof Integer) {
+            StringCalculatorTestUtils.assertResultEquals(message, input, (Integer) expectedResult);
+        }
+        else if (expectedResult instanceof Class) {
+            StringCalculatorTestUtils.assertRaises(message, input, (Class) expectedResult);
+        }
+        else {
+            throw new IllegalArgumentException("Unexpected value for expectedResult. Type = ["+expectedResult.getClass().getName()+"]. Value = ["+expectedResult+"]");
+        }
+    }
+
+}
--- src/test/java/com/sosuna/formacion/tdd/katas/stringcalculator/StringCalculatorTestUtils.java
+++ src/test/java/com/sosuna/formacion/tdd/katas/stringcalculator/StringCalculatorTestUtils.java
@@ -1,13 +1,33 @@
 package com.sosuna.formacion.tdd.katas.stringcalculator;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-/**
- * Created by Hexad Spain on 09/03/2017.
- */
-public class StringCalculatorTestUtils {
-    static void assertResultEquals(String message, String test, int expectedResult) {
-        int result = StringCalculator.calculate(test);
+class StringCalculatorTestUtils {
+    static void assertResultEquals(String message, String input, int expectedResult) {
+        int result = StringCalculator.calculate(input);
         assertEquals(message, expectedResult, result);
+    }
+
+    static void assertRaises(String message, String input, Class<? extends Throwable> expectedToRaise) {
+        try {
+            int result = StringCalculator.calculate(input);
+            fail(
+                    (message!=null? (message + ". ") : "")
+                    + "Expected to raise a Throwable of type ["+expectedToRaise.getName()+"], but returned with result ["+result+"]"
+            );
+        }
+        catch (Throwable raised) {
+            if (expectedToRaise.isAssignableFrom(raised.getClass())) {
+                // Raised Throwable type matches with expected type
+                // Test passes.
+                return;
+            }
+            else {
+                fail(
+                        (message!=null? (message + ". ") : "")
+                        + "Expected to raise a Throwable of type ["+expectedToRaise.getName()+"], but the type was  ["+raised.getClass().getName()+"]"
+                );
+            }
+        }
     }
 }
