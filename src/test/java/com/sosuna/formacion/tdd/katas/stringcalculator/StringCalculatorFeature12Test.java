9ff03929d95580adb4cab68b08447d01c19fefa7
XYZ <idm.caz@mailinator.com>
2017-03-10 09:40:35 +0100

    Feature #11: Logging failure notification webservice Test spec

---
+++ src/test/java/com/sosuna/formacion/tdd/katas/stringcalculator/StringCalculatorFeature12Test.java
@@ -0,0 +1,43 @@
+package com.sosuna.formacion.tdd.katas.stringcalculator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Feature #12: Logging failure webservice
+ */
+public class StringCalculatorFeature12Test {
+
+    private StringCalculator calculator;
+    private ILogger logger;
+    private ILoggerFailureNotificationWebService loggerFailWS;
+
+    @Before
+    public void setUp() {
+        logger = mock(ILogger.class);
+        loggerFailWS = mock(ILoggerFailureNotificationWebService.class);
+
+        calculator = new StringCalculator(logger, loggerFailWS);
+    }
+
+    @Test
+    public void calculatorShouldNotifyWebServiceWhenLoggingFails() {
+        final String LOG_MESSAGE = "Error writing to log output"
+
+        // Throw IOException when the logger is called to log the result
+        doThrow(new IOException(LOG_MESSAGE)).when(logger).write(anyString());
+
+        calculator.calculate("1,2,3");
+
+        // Expect the logger failure WS to be called with the exception message
+        verify(loggerFailWS).notifyLoggingFailure(LOG_MESSAGE);
+    }
+
+}
