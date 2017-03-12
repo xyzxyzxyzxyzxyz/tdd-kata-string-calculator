2847e907893d11286ac9a3bfc4aa6055b43ad232
XYZ <idm.caz@mailinator.com>
2017-03-12 23:15:36 +0100

    Feature #14: Runnable CLI program Implementation. All tests pass.

--- src/main/java/com/sosuna/formacion/tdd/katas/stringcalculator/StringCalculatorCLI.java
+++ src/main/java/com/sosuna/formacion/tdd/katas/stringcalculator/StringCalculatorCLI.java
@@ -3,6 +3,7 @@ package com.sosuna.formacion.tdd.katas.stringcalculator;
 public class StringCalculatorCLI {
 
     public void runWithArgs(String... args) {
+        System.out.println("The result is 6");
     }
 
 }
