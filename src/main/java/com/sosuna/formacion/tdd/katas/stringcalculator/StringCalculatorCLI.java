747b0caf233dcc563be71c179001e299dade4508
XYZ <idm.caz@mailinator.com>
2017-03-13 00:18:27 +0100

    Feature #14: Runnable CLI program Final implementation. All tests pass.

--- src/main/java/com/sosuna/formacion/tdd/katas/stringcalculator/StringCalculatorCLI.java
+++ src/main/java/com/sosuna/formacion/tdd/katas/stringcalculator/StringCalculatorCLI.java
@@ -2,8 +2,66 @@ package com.sosuna.formacion.tdd.katas.stringcalculator;
 
 public class StringCalculatorCLI {
 
+    private StringCalculator calculator;
+
+    StringCalculatorCLI(StringCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    /**
+     * Instance execution method, more friendly to
+     * dependency injection
+     *
+     * @param args
+     */
     public void runWithArgs(String... args) {
-        System.out.println("The result is 6");
+        // Retrieve argument from command line
+        String calculatorInput = args[0];
+        // Remove surrounding '' from argument
+        calculatorInput = calculatorInput.substring(1, calculatorInput.length()-1);
+        // Execute calculator
+        int result = calculator.calculate(calculatorInput);
+        // Display result message
+        System.out.println("The result is " + result);
+    }
+
+    /**
+     * Static execution method that instantiates a CLI with
+     * a default calculator and executes it with the input.
+     *
+     * @param args
+     */
+    public static void main(String... args) {
+        // Create calculator
+        StringCalculator calculator = createCalculator();
+
+        // Create calculator CLI
+        StringCalculatorCLI cli = new StringCalculatorCLI(calculator);
+
+        // Run CLI
+        cli.runWithArgs(args);
+    }
+
+    private static StringCalculator createCalculator() {
+        // Instantiate the calculator
+        StringCalculator calculator = new StringCalculator(
+                // Kata doesn't say where should we dump the log output
+                new ILogger() {
+                    @Override
+                    public void write(String message) {
+                        // NO-OP
+                    }
+                },
+                // Kata doesn't say that we need to implement the webservice
+                new ILoggerFailureNotificationWebService() {
+                    @Override
+                    public void notifyLoggingFailure(String message) {
+                        // NO-OP
+                    }
+                }
+        );
+
+        return calculator;
     }
 
 }
--- src/test/java/com/sosuna/formacion/tdd/katas/stringcalculator/StringCalculatorFeature14Test.java
+++ src/test/java/com/sosuna/formacion/tdd/katas/stringcalculator/StringCalculatorFeature14Test.java
@@ -6,25 +6,49 @@ import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class StringCalculatorFeature14Test {
 
     @Rule
     public final SystemOutRule consoleOutput = new SystemOutRule().enableLog();
 
+    private StringCalculator calculator;
     private StringCalculatorCLI cli;
 
     @Before
     public void setUp() throws Exception {
-         cli = new StringCalculatorCLI();
+        // Mock the calculator
+        calculator = mock(StringCalculator.class);
+        // Build CLI with mock calculator
+        cli = new StringCalculatorCLI(calculator);
     }
 
     @Test
-    public void executingProgramWithInput123ShouldPrintOutMessageWithResult6() throws Exception {
-        cli.runWithArgs("'1,2,3'");
-        assertConsoleOutputEquals("The result is 6");
-    }
+    public void executingProgramWithInput123ShouldCallCalculatorAndPrintOutMessageWithResult() throws Exception {
+        // Mock calculator behaviour
+        final String CALC_IN = "1,2,3";
+        final int CALC_OUT = 6;
+        when(calculator.calculate(CALC_IN)).thenReturn(CALC_OUT);
 
+        /*
+            Run CLI with input
+            Though the program executes with the static "main" execution
+            method, that method simply instantiates a CLI instance and
+            executes the runWithArgs() instance method.
+            Thus, we can assume that testing the instance execution method we
+            are also testing the static execution method.
+         */
+        cli.runWithArgs("'"+CALC_IN+"'");
+
+        // Verify calculator has been called appropriately
+        verify(calculator).calculate(CALC_IN);
+
+        // Compare expected vs actual output
+        assertConsoleOutputEquals("The result is " + CALC_OUT);
+    }
 
 
 
